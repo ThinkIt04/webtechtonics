850d95f063e9ba7b7a4913fd49c32176887bf4a4
ThinkIt04 <72980564+ThinkIt04@users.noreply.github.com>
2021-01-24 20:05:00 +0530

    ..........

---
+++ SCRIPT/script.js
@@ -0,0 +1,6 @@
+function menu(){
+    document.getElementById('menu1').style.transform = "translateX(100%)";
+}
+function menux(){
+    document.getElementById('menu1').style.transform = "translateX(0%)";
+}
