b9904d55741ce3623535353a6668d773ca9717b1
ThinkIt04 <72980564+ThinkIt04@users.noreply.github.com>
2021-02-03 11:46:57 +0530

    .........

--- SCRIPT/script.js
+++ SCRIPT/script.js
@@ -4,3 +4,13 @@ function menu(){
 function menux(){
     document.getElementById('menu1').style.transform = "translateX(0%)";
 }
+const scriptURL = 'https://script.google.com/macros/s/AKfycbx2KV-EZUTPe-WbxMSqg1O1H22TcizcjBW3MauczuSQazfKU3aG/exec';
+const form = document.forms['google-sheet'];
+const alt = document.getElementById("t-alert");
+const eralt = document.getElementById("e-alert");
+form.addEventListener('submit', e => {
+        e.preventDefault()
+        fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+            .then(response => {alt.style.display = "block";})
+            .catch(error => {eralt.style.display = "block";})
+})
